fd570339ad721e27bd215b1ba8d37e1201586e46
emilyr27-dot <emilyr27@downtownschoolseattle.org>
2026-01-16 08:50:42 -0800

    Add minimal service worker

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open("debateos").then(cache =>
-      cache.addAll(["./", "./index.html"])
-    )
-  );
+// Minimal service worker for PWA
+self.addEventListener('install', event => {
+  console.log('Service worker installed');
+});
+
+self.addEventListener('fetch', event => {
+  // This keeps the service worker active but doesn't cache anything yet
 });
