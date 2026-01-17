68065d7155be7c60034987c6c448167b30eb3c50
emilyr27-dot <emilyr27@downtownschoolseattle.org>
2026-01-17 14:11:56 -0800

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,23 @@ function saveData(key, data) {
 function generateId() {
   return '_' + Math.random().toString(36).substr(2, 9);
 }
+function getFolders() {
+  return getData("folders");
+}
+
+function saveFolders(folders) {
+  saveData("folders", folders);
+}
+
+function createFolder(name) {
+  const folders = getFolders();
+  const folder = {
+    id: generateId(),
+    name,
+    caseIds: []
+  };
+  folders.push(folder);
+  saveFolders(folders);
+  return folder;
+}
+
